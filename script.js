2e312da33d8dfbe9c9fc2b23f5ae78a4cb37160f
samh7 <samh7@gmail.com>
2024-08-17 01:40:18 +0300

    imported fs

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 const path = require("path");
 const readline = require("readline");
-
+const fs = require('fs');
 function createHTMLBoilerplate(title) {
   return `
 <!DOCTYPE html>
