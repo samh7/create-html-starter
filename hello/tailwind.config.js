f89c513ed2122e56cdc858c80a67e7f91e7d16af
samh7 <samh7@gmail.com>
2024-08-17 10:47:45 +0300

    Added some dependancies

---
+++ hello/tailwind.config.js
@@ -0,0 +1,10 @@
+
+  /** @type {import('tailwindcss').Config} */
+    module.exports = {
+      content: ["./*.{js,html}"],
+      theme: {
+        extend: {},
+      },
+      plugins: [],
+  }
+  
